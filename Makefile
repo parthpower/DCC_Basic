54a0984e6ef66ccc54ab04b7445ebbf2c3cae0aa
Parth Parikh <parthpower@gmail.com>
2018-06-13 22:01:16 -0700

    Fixed search path issue. Seperated TCL files

--- Makefile
+++ Makefile
@@ -3,4 +3,5 @@ all:
 	mkdir reports
 	dc_shell -f script/dc_script.tcl
 clean:
-	rm -rf reports/* design_data/*
+	rm -rf reports/ design_data/
+	
---
+++ script/setup_lib.tcl
@@ -0,0 +1,8 @@
+# Setup the libraries
+####################################################
+# Set Search Path
+set search_path "$search_path ref/models/"
+# Set Target Library and Link Library
+set target_library "saed90nm_typ.db"
+set link_library "* saed90nm_typ.db"
+####################################################
